1fd703cb01bd0680b38b33af5c3f274702154d3a
Vicky Abell <vicky@OEL0001.local>
2018-05-11 21:28:07 +0100

    remov console log, add menu colour to homepage

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -2,8 +2,6 @@ import React from 'react';
 import HeaderContainer from '../../components/HeaderContainer'
 import data from '../../data/homepage.json'
 
-console.log(data);
-
 const Home = () => (
   <HeaderContainer text={data.title} image={data.headerImage} />
 );
