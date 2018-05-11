e5c9e65aa895558d90a774ac185f38d9ca76edde
Vicky Abell <vicky@OEL0001.local>
2018-05-11 20:59:10 +0100

    adding header image to homepage

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import HeaderContainer from '../../components/HeaderContainer'
+import data from '../../data/homepage.json'
+
+console.log(data);
 
 const Home = () => (
-  <HeaderContainer text='hi' image='' />
+  <HeaderContainer text={data.title} image='' />
 );
 
 export default Home;
