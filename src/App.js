aa47565a1a7b9e4bd5b9e652236d780328dcfab3
Joseph Abell <joseph.abell@piksel.com>
2018-05-23 22:51:09 +0100

    remove unneeded css, fix line spacing

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import Home from './pages/Home/index';
-
+    
 class App extends Component {
   render() {
     return (
