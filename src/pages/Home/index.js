bde9d4fefc4d2b83ff36cf3c052018f60d867f68
Vicky Abell <vicky@OEL0001.local>
2018-05-28 19:43:46 +0100

    move homepage to it's own section, make pages reusable

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import HeaderContainer from '../../components/HeaderContainer'
-import data from '../../data/pages/homepage.json'
+import data from '../../data/homepage.json'
 
 const Home = () => (
   <HeaderContainer text={data.title} image={data.headerImage} />
