5128b2a01ea0425b6af8642655946b1804b6a35a
Vicky Abell <vicky@OEL0001.local>
2018-06-06 20:11:48 +0100

    rename ctaMap

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -4,11 +4,24 @@ import Menu from '../../components/Menu';
 import homepageData from '../../data/homepage.json';
 import menuData from '../../data/menu.json';
 
-const Home = () => (
-  <div>
-    <HeaderContainer text={homepageData.title} image={homepageData.headerImage} />
-    <Menu menu={menuData.menu} menuColour={homepageData.menuColour} />
-  </div>
+import './style.css';
+
+const ctaMap = ({ link, title, deck, image }) => (
+  <a href={link}>
+    <h2>{title}</h2>
+    <p>{deck}</p>
+    <img src={image} alt='' />
+  </a>
 );
+
+const Home = () => {
+  return (
+    <div>
+      <HeaderContainer text={homepageData.title} image={homepageData.headerImage} />
+      <Menu menu={menuData.menu} menuColour={homepageData.menuColour} />
+      { homepageData.cta.map(ctaMap) }
+    </div>
+  );
+};
 
 export default Home;
