f16d64f5d6d6fb3138f3c2b7e7f4cbb8672922b5
Joseph Abell <joseph.abell@piksel.com>
2018-06-07 12:06:01 +0100

    fixed header tests, added a test as an example

--- src/pages/Home/home.test.js
+++ src/pages/Home/home.test.js
@@ -1,12 +1,20 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { mount } from 'enzyme';
+
 import Home from './index';
 
-it('renders without crashing', () => {
-  shallow(<Home />);
-});
+let component;
 
-it('contains HeaderComponent', () => {
-  const result = shallow(<Home />);
-  expect(result.find('div')).to.have.length(1);
+describe('Homepage', () => {
+  beforeEach(() => {
+    component = mount(<Home />);
+  });
+
+  it('renders without crashing', () => {
+    expect(component).toBeDefined();
+  });
+
+  it('contains a header', () => {
+    expect(component.find('HeaderContainer')).toExist();
+  });
 });
