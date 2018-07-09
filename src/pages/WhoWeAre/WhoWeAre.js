f347be5d5f3edab7888b381e25e61ee980cd3be5
Vicky Abell <vicky@OEL0001.local>
2018-07-09 22:03:10 +0100

    added subtitles, descriptions, and contents to the who-we-are page

--- src/pages/WhoWeAre/WhoWeAre.js
+++ src/pages/WhoWeAre/WhoWeAre.js
@@ -1,29 +1,74 @@
 import React from 'react';
 import Async from 'react-promise';
 import { url } from '../../config';
-
 import Header from '../../components/HeaderContainer';
+
+const Content = ({ content }) => (
+  <li>
+    <div className='left'>
+      <div>{content.left.deck}</div>
+      <div>{content.left.image}</div>
+      <div>{content.left.colour}</div>
+    </div>
+    <div className='right'>
+      <div>{content.right.deck}</div>
+      <div>{content.right.image}</div>
+      <div>{content.right.colour}</div>
+    </div>
+  </li>
+);
+
+const Contents = ({ contents }) => (
+  <ul>
+    { contents.map(({content}) => (
+      <Content content={content} />
+    )) }
+  </ul>
+);
+
 const WhoWeAre = () => (
   <Async
     promise={new Promise(async (resolve) => {
       const whoWeAreResponse = await fetch(url + 'data/pages/who-we-are.json');
-      const { title, header } = await whoWeAreResponse.json();
+      const { title, header, subtitle, deck, contents } = await whoWeAreResponse.json();
       const { image, menuColour } = header;
-  
+      const subtitleText = subtitle.subtitle;
+      const subtitleImage = url + subtitle.image.slice(1);
+      const deckTitle = deck.title;
+      const deckParagraph = deck.paragraph;
+      const deckColour = deck.colour;
+
       resolve({
-        header: {
-          title,
-          image,
-          menuColour
-        }
-      });  
+        title,
+        image: url + image.slice(1),
+        subtitleImage,
+        subtitleText,
+        deckTitle,
+        deckParagraph,
+        deckColour,
+        contents
+      });
     })}
 
-    then={({title, image}) => (
-      <Header
-        text={title}
-        image={url + image}
-      />
+    then={({
+      title,
+      image,
+      subtitleImage,
+      subtitleText,
+      deckTitle,
+      deckParagraph,
+      deckColour,
+      contents
+    }) => (
+      <div>
+        <Header
+          text={title}
+          image={image}
+        />
+        <img src={subtitleImage} alt='' />
+        {deckTitle}
+        <Contents contents={contents} />
+      </div>
     )}
   />
 );
