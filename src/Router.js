7d68bd769ca3c69ebc66531f5cd9cd1931bbaf50
Vicky Abell <vicky@OEL0001.local>
2018-07-11 22:41:57 +0100

    add 404 error page

--- src/Router.js
+++ src/Router.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import Home from './pages/Home';
+import Error404 from './pages/Error404';
 import PageTemplate from './components/PageTemplate';
     
 class Router extends Component {
@@ -10,6 +11,7 @@ class Router extends Component {
       <BrowserRouter>
         <Switch>
           <Route exact path='/' component={Home} />
+          <Route exact path='/404' component={Error404} />
           <Route component={PageTemplate} />
         </Switch>
       </BrowserRouter>
