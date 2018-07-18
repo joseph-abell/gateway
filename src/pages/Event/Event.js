c6ecaa2972c32f39d75e78c7eaf9aa8876a9bf7b
Vicky Abell <vicky@OEL0001.local>
2018-07-18 21:00:46 +0100

    built event page ish

--- src/pages/Event/Event.js
+++ src/pages/Event/Event.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import Async from 'react-promise';
+import { url } from '../../config.js';
 
 const Event = () => (
   <Async
     promise={new Promise(async (resolve) => {
-      resolve();
+      const response = await fetch(url + 'data/events/new-event.json');
+      const eventData = await response.json();
+      resolve({
+        title: eventData.title,
+        image: eventData.image,
+        header: eventData.header,
+        date: eventData.date,
+        time: eventData.time,
+        deck: eventData.deck
+      });
     })}
-    then={() => (
+    then={({title, image, header, date, time, deck}) => (
       <div/>
     )} />
 );
