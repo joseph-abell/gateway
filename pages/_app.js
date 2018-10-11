adb8726f927dc3572836cbfa8d4c12714d07e26e
Joseph Abell <joseph.abell@piksel.com>
2018-10-11 22:30:47 +0100

    add breakpoints provider

--- pages/_app.js
+++ pages/_app.js
@@ -1,26 +1,36 @@
 import App, { Container } from 'next/app';
 import Head from 'next/head';
 import React from 'react';
+import { BreakpointsProvider } from 'react-with-breakpoints';
 
 class Gateway extends App {
   render() {
     const { Component, pageProps } = this.props;
+    const breakpoints = {
+      small: 768,
+      medium: 991,
+      large: 1021,
+      xlarge: 1221
+    };
+
     return (
-      <Container>
-        <Head>
-          <link href="/static/slick.css" rel="stylesheet" />
-          <link href="/static/slick-theme.css" rel="stylesheet" />
-          <link href="/static/rc-slider.css" rel="stylesheet" />
-          <link href="/static/index.css" rel="stylesheet" />
-          <link
-            rel="shortcut icon"
-            href="/static/favicon.ico"
-            type="image/x-icon"
-          />
-          <title key="title">Gateway Church, York</title>
-        </Head>
-        <Component {...pageProps} />
-      </Container>
+      <BreakpointsProvider breakpoints={breakpoints}>
+        <Container>
+          <Head>
+            <link href="/static/slick.css" rel="stylesheet" />
+            <link href="/static/slick-theme.css" rel="stylesheet" />
+            <link href="/static/rc-slider.css" rel="stylesheet" />
+            <link href="/static/index.css" rel="stylesheet" />
+            <link
+              rel="shortcut icon"
+              href="/static/favicon.ico"
+              type="image/x-icon"
+            />
+            <title key="title">Gateway Church, York</title>
+          </Head>
+          <Component {...pageProps} />
+        </Container>
+      </BreakpointsProvider>
     );
   }
 }
